c3fae5b7a6ed41de2532eec15dbeaec9a8b6842a
mohammedihajjaj <mohammedih31@gmail.com>
2020-12-18 10:46:05 +0200

    Adding reset to ColorPicker

--- javascript.js
+++ javascript.js
@@ -10,3 +10,14 @@ secondaryColor.addEventListener("change", () => {
   let sColor = secondaryColor.value;
   document.getElementById("basic-color").style.setProperty("--color1", sColor);
 });
+
+function resetColors() {
+  mainColor.value = "#e958a2";
+  document
+    .getElementById("basic-color")
+    .style.setProperty("--color2", "#e958a2");
+  secondaryColor.value = "#9923a9";
+  document
+    .getElementById("basic-color")
+    .style.setProperty("--color1", "#9923a9");
+}
